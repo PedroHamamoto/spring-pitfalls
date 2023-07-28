f8c4f82c2f22a39f78ee56d92de63617dac6724c
PedroHamamoto <hamamoto.pedro@gmail.com>
2023-07-27 22:43:15 -0300

    First commit

---
+++ src/main/kotlin/br/com/hamamoto/spring_traps/SpringTrapsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.hamamoto.spring_traps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTrapsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringTrapsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/hamamoto/spring_traps/SpringTrapsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.hamamoto.spring_traps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTrapsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
