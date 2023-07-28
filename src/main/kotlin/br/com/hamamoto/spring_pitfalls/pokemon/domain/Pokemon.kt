f43363caf32a3e9367ee453d1ca0e2cd060bd6cb
PedroHamamoto <pedro.hamamoto@inventa.shop>
2023-07-28 17:44:50 -0300

    Imporoving json order

--- src/main/kotlin/br/com/hamamoto/spring_pitfalls/pokemon/domain/Pokemon.kt
+++ src/main/kotlin/br/com/hamamoto/spring_pitfalls/pokemon/domain/Pokemon.kt
@@ -1,7 +1,7 @@
 package br.com.hamamoto.spring_pitfalls.pokemon.domain
 
 data class Pokemon(
+    val id: Int,
     val name: String,
-    val abilities: List<String>,
-    val id: Int
+    val abilities: List<String>
 )
