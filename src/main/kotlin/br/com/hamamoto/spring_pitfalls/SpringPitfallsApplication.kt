06a5d4caa9a1713108deb292435aa673225c6a1b
PedroHamamoto <hamamoto.pedro@gmail.com>
2023-07-27 23:11:25 -0300

    Creating a simple pokemon client

--- src/main/kotlin/br/com/hamamoto/spring_pitfalls/SpringPitfallsApplication.kt
+++ src/main/kotlin/br/com/hamamoto/spring_pitfalls/SpringPitfallsApplication.kt
@@ -2,8 +2,10 @@ package br.com.hamamoto.spring_pitfalls
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class SpringPitfallsApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/br/com/hamamoto/spring_pitfalls/pokemon/client/PokeApiClient.kt
@@ -0,0 +1,13 @@
+package br.com.hamamoto.spring_pitfalls.pokemon.client
+
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@FeignClient(url = "\${poke-api.url}", name = "poke-api")
+interface PokeApiClient {
+
+    @GetMapping("/api/v2/pokemon/{name}")
+    fun getByName(@PathVariable("name") name: String): Map<String, Any?>
+
+}
