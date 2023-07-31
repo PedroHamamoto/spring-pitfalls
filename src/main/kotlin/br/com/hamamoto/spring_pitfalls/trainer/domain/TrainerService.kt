59d8e66e3454a2f567c005fcd6f8be9b8e5146ed
PedroHamamoto <pedro.hamamoto@inventa.shop>
2023-07-31 12:05:23 -0300

    Demonstring the proxy pitfall on TrainerService

--- src/main/kotlin/br/com/hamamoto/spring_pitfalls/trainer/domain/TrainerService.kt
+++ src/main/kotlin/br/com/hamamoto/spring_pitfalls/trainer/domain/TrainerService.kt
@@ -1,12 +1,27 @@
 package br.com.hamamoto.spring_pitfalls.trainer.domain
 
 import br.com.hamamoto.spring_pitfalls.trainer.data.TrainerRepository
+import java.lang.RuntimeException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TrainerService(
     private val trainerRepository: TrainerRepository
 ) {
-    fun create(trainer: Trainer) =
+
+    fun create(trainer: Trainer): Trainer {
+        val persistedTrainer = save(trainer)
+        doSomething()
+
+        return persistedTrainer
+    }
+
+    @Transactional
+    private fun save(trainer: Trainer) =
         trainerRepository.save(trainer)
+
+    private fun doSomething() {
+        throw RuntimeException("Unknown error")
+    }
 }
